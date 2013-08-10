17dc6df5ed143f7be00bdade23f7d071e73af218
Pez Cuckow <email@pezcuckow.com>
2013-08-11 00:49:32 +0100

    Add shared folder

---
+++ gamemode/init.lua
@@ -0,0 +1,4 @@
+--- This file is only run on the server
+
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
---
+++ gamemode/shared.lua
@@ -0,0 +1,4 @@
+GM.Name = "BoxWar"
+GM.Author = "Need4Beans"
+GM.Email = "email@pezcuckow.com"
+GM.Website = "www.need4beans.com
