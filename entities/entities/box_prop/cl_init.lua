7dd11bf2f4bd563541abcbcc4362d81a4f3677ee
Pez Cuckow <email@pezcuckwow.com>
2013-08-16 12:05:29 +0100

    Attempt to fix hitboxes

--- entities/entities/box_prop/cl_init.lua
+++ entities/entities/box_prop/cl_init.lua
@@ -9,34 +9,14 @@ include("shared.lua")
 	end
 end]]
 
-local lastYaw = 0;
-
 function ENT:Draw()
 	local pl = self:GetOwner()
 	
 	if pl:IsValid() then
-		-- don't draw the hat on the local player if they are in firstperson mode
+		-- don't draw the local player if they are in firstperson mode
 		if LocalPlayer() == pl and not LocalPlayer():KeyDown(IN_ATTACK2) and not LocalPlayer():ShouldDrawLocalPlayer() then
 			return
 		end
-	
-		local m = Matrix()
-				
-		-- If the player is holding down attack 2 don't rotate
-		if(pl:KeyDown(IN_ATTACK2) && pl:GetVelocity():Length() == 0) then
-			m:SetAngles(Angle(0, lastYaw, 0)) --only the yaw (rotation in z)
-		else
-			local angles = pl:GetAngles()
-			m:SetAngles(Angle(0, angles.y, 0)) --only the yaw (rotation in z)
-			lastYaw = angles.y
-		end
-		
-		-- Set to player pos and add 20 in z
-		m:SetTranslation(pl:GetPos())
-		m:Translate(Vector(0, 0, -self:OBBMins().z))
-		
-		self:SetRenderOrigin(m:GetTranslation())
-		self:SetRenderAngles(m:GetAngles())
 	end
 	
 	self:DrawModel()
--- entities/entities/box_prop/shared.lua
+++ entities/entities/box_prop/shared.lua
@@ -1,3 +1,33 @@
 // Entity information.
 ENT.Type = "anim"
 ENT.Base = "base_anim"
+
+local min, max
+
+function ENT:Initialize()
+	min = self:OBBMins()
+	max = self:OBBMaxs()
+end
+
+local lastYaw = 0;
+
+function ENT:Think() 
+	local pl = self:GetOwner()
+	
+	if pl:IsValid() then
+
+		-- Shift the box up by it's min Z position
+		self:SetPos(self:GetOwner():GetPos() + Vector(0,0,-min.z))
+			
+		-- If the player is holding down attack 2 don't rotate
+		if(pl:KeyDown(IN_ATTACK2) && pl:GetVelocity():Length() == 0) then
+			self:SetAngles(Angle(0, lastYaw, 0)) --only the yaw (rotation in z)
+		else
+			local angles = pl:GetAngles()
+			self:SetAngles(Angle(0,angles.y,0))
+			lastYaw = angles.y
+		end
+	
+	end
+	
+end
