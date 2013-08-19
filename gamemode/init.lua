33bc5f55bf5410cd7c3662e7acef440f12ccbf82
Pez Cuckow <email@pezcuckow.com>
2013-08-19 12:01:55 +0100

    Move player spawn code to the player class

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,23 +1,27 @@
+---
+-- This script and all included files
+-- Copyright 2013 Pez Cuckow & Oliver Brown.
+-- All rights reserved.
+---
+-- Main server side script, sends files to client and includes others
+---
+
 -- Code for the client
 AddCSLuaFile("sh_init.lua")
-AddCSLuaFile("player_class/class_box.lua")
+AddCSLuaFile("players/class_box.lua")
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("sh_player.lua")
+
+-- 'Global' variables
+DEBUG_MODE = true
 
 -- Run the shared file
 include("sh_init.lua")
 
 -- Load server only files
-include("maze.lua")
-include("lib/json.lua")
-include("toolkit.lua")
-
--- Currently in debug?
-debugMode = true
-
--- Count how many boxes there are
-totalMazeBoxes = 0
-currentMazeBoxes = 0
+include("maze/maze.lua") -- generate mazes
+include("lib/json.lua") -- parse json
+include("lib/toolkit.lua") -- add lua functions
 
 -- Called when the gamemode is initialized.
 function Initialize()
@@ -25,70 +29,20 @@ function Initialize()
 	
 	-- See Maze.Lua
 	timer.Simple(1,CreateMazes)
-	
 end
 hook.Add("Initialize", "BoxWar_Initialize", Initialize)
 
 -- Called when a player leaves.
 function PlayerDisconnected(pl)
-	pl:RemoveProp()
+	pl:RemoveProp() -- just delete their prop
 end
 hook.Add("PlayerDisconnected", "BoxWar_PlayerDisconnected", PlayerDisconnected)
 
 -- If someone dies, delete their prop!
 function PlayerDied( player, weapon, killer )
-	-- Create an invisible prop to copy
-    local invisibleDoll = ents.Create("prop_physics")
-    invisibleDoll:SetCollisionGroup( COLLISION_GROUP_WEAPON )
-    invisibleDoll:SetModel("models/Humans/Group01/Male_05.mdl")
-    invisibleDoll:SetAngles(player:GetAngles())
-    invisibleDoll:SetPos( player:GetPos() )
-    invisibleDoll:SetColor(Color(0, 0, 0, 0)) 
-    invisibleDoll:Spawn()
-
-    -- Set the invisible prop to crouch
-    sequence = invisibleDoll:LookupSequence("roofidleone")
-	invisibleDoll:SetPlaybackRate( 1.0 )
-	invisibleDoll:SetSequence(sequence)
-	invisibleDoll:ResetSequence( sequence )
-	invisibleDoll:SetCycle( 1 )
-    
-    -- Spawn a ragdoll for the player
-    local doll = ents.Create("prop_ragdoll")
-    doll:SetParent(invisibleDoll)
-    doll:AddEffects(EF_BONEMERGE)
-    doll:SetModel( "models/Humans/Group01/Male_05.mdl" )
-    --doll:SetModel( player:GetModel() )
-    doll:SetPos( player:GetPos() )
-    doll:SetAngles( player:GetAngles() )
-    doll:Spawn()
-    doll:SetCollisionGroup( COLLISION_GROUP_WEAPON )
-    
-    -- Link the ragdoll to the invisible, then remove the invisible
-    doll:SetParent()
-    invisibleDoll:Remove()
-    
-    -- Smash the box
-	if (player.prop ~= nil)	then	
-		player.prop:PrecacheGibs()
-		player.prop:GibBreakClient(Vector(0,0,100))
-	end
-	
-	-- Safely remove the doll after 60 seconds
-    SafeRemoveEntityDelayed(doll, 60)
-				  
-    -- Delete the prop
-	player:RemoveProp()
+	pl:BoxWarkill() -- run the kill effect
 end
 hook.Add( "PlayerDeath", "BoxWar_PlayerDeath", PlayerDied )
-
--- Called whenever a player spawns and has auth
-function GM:PlayerAuthed( pl )
-	SetBox(pl)
-	
-	-- Base gamemode
-	self.BaseClass:PlayerAuthed(pl)
-end
 
 -- On the first player spawn
 function GM:PlayerInitialSpawn(pl)
@@ -108,129 +62,18 @@ function GM:PlayerSpawn( pl )
 	SetBox(pl)
 end
 
-function GM:CanStartRound( iNum )
-     --if team.NumPlayers( TEAM_RED ) > 0 and team.NumPlayers( TEAM_BLUE ) > 0 then
-          return true
-     --else
-     --     return false
-     --end
-end
-
 -- THIS DOESN'T WORK YET
-function GM:OnRoundStart() 
-
-	
+function GM:OnRoundStart() 	
 	for _,p in ipairs(player.GetAll()) do p:PrintMessage( HUD_PRINTCENTER, "A new round has started!" ) end
 end
---[[ Called whenever a player spawns and must choose a model.
-function GM:PlayerSetModel( pl )
-	player_manager.SetPlayerClass(pl, "player_box")
-end]]
 
---[[function GM:PlayerConnect( name, ip )
-	print("Player" .. name .. " has joined the game.")
-end
---]]
 
--- We use info_player_axis as no map should have these defined
-local SpawnTypes = {"info_player_axis"}
-
-function GetSpawnEnts(shuffled, force_all)
-   local tbl = {}
-   for k, classname in pairs(SpawnTypes) do
-      for _, e in pairs(ents.FindByClass(classname)) do
-         if IsValid(e) and (not e.BeingRemoved) then
-            table.insert(tbl, e)
-         end
-      end
-   end
-
-   if shuffled then
-      table.Shuffle(tbl)
-   end
-
-   return tbl
-end
-
+-- Called just before a player spawns to get their spawn point
 function GM:PlayerSelectSpawn( pl )
-    spawns = GetSpawnEnts()    
-    spawnNumber = math.random( #spawns )
+    spawns = GetSpawnEnts()
     
-    return spawns[spawnNumber]
+    return spawns[math.random( #spawns )]
 end
-
-	
---[[function GM:KeyPress( pl, key )	
-	 if ( key == IN_ATTACK ) then
-	 	Player:KeyDownLast( number key )
-		pl.angerLevel = pl.angerLevel + 1
-		print(pl.angerLevel)
-	 end
-end]]
-
--- Set a player to be a box
-function SetBox ( pl ) 
-
-	-- If the player hasn't been spawned already
-	if(!pl.spawned) then	
-		pl.spawned = true
-	
-		-- First set the player model to a small model
-		local player_model = "models/Gibs/Antlion_gib_small_3.mdl"
-		--local player_model = "models/props_junk/wood_crate001a.mdl"
-		
-	
-		--util.PrecacheModel(player_model)
-		pl:SetModel(player_model)
-		pl:SetRenderMode(RENDERMODE_TRANSALPHA)
-		pl:SetColor(Color(255, 255, 255, 1))
-		--pl:SetRenderOrigin(Vector(0, 0, 20))
-		--pl:SetLocalPos(Vector(0,0,20))
-		
-		-- Then spawn a new box around them
-		pl.prop = ents.Create("box_prop")
-		--pl.prop:SetNotSolid(true)
-		
-		pl.prop:SetOwner(pl)
-		pl.prop:SetPos(pl:GetPos())
-		pl.prop:SetAngles(pl:GetAngles())
-		pl.prop:SetSolid(SOLID_BBOX)
-		pl.prop:SetParent(pl)
-		pl.prop:Spawn()
-		
-		pl.angerLevel = 0;
-	
-		-- Set initial player max health.
-		pl.prop.max_health = 100
-		pl.prop.health = 100
-		
-		-- Set player info
-		pl:SetJumpPower( 250 )
-		pl:SetRunSpeed(250) --default 500
-		pl:SetWalkSpeed(125) --default 250
-		pl:DrawWorldModel(false);
-		pl:SetHealth(100)
-		
-		pl:SetViewOffset( 	Vector( 0, 0, 25 ) ) -- I think this must base it on the prop so we must round up
-		pl:SetViewOffsetDucked( Vector( 0, 0, 36+25 ) )
-		
-		-- Calculate new player hull slightly smaller than prop
-		local hull_xy_max 	= math.floor(math.Max(pl.prop:OBBMaxs().x, pl.prop:OBBMaxs().y) * 0.8)
-		local hull_xy_min 	= hull_xy_max * -1
-		local hull_z 		= math.floor(pl.prop:OBBMaxs().z * 0.8)
-		
-		-- Set player hull.
-		pl:SetHull(Vector(hull_xy_min, hull_xy_min, 0), Vector(hull_xy_max, hull_xy_max, hull_z))
-		pl:SetHullDuck(Vector(hull_xy_min, hull_xy_min, 0), Vector(hull_xy_max, hull_xy_max, hull_z))
-	
-		-- Set the player hull client side
-		umsg.Start("SetHull", pl)
-			umsg.Long(hull_xy_max)
-			umsg.Long(hull_z)
-		umsg.End()
-	end
-end
-
 
 -- Called when an entity takes damage.
 --[[function EntityTakeDamage( target, dmginfo )
